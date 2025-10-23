10fd66469247d2ac8b1c5dd38e332a03b49e6e80
rnsa357-stack <rnsa357@gmail.com>
2025-10-23 21:39:04 +0530

    Add array utility functions in arraylib.c
    Implemented several array utility functions including finding max/min indices, calculating average, displaying, reversing, sorting, and linear searching.

---
+++ arraylib.c
@@ -0,0 +1,52 @@
+#include "arraylib.h"
+#include <stdio.h>
+
+int findMaxIndex(int arr[], int size) { // #Cp
+    int max = 0;
+    for (int i = 1; i < size; i++)
+        if (arr[i] > arr[max]) max = i;
+    return max;
+}
+
+int findMinIndex(int arr[], int size) { // #Cp
+    int min = 0;
+    for (int i = 1; i < size; i++)
+        if (arr[i] < arr[min]) min = i;
+    return min;
+}
+
+float findAverage(int arr[], int size) { // #Cp
+    int sum = 0;
+    for (int i = 0; i < size; i++) sum += arr[i];
+    return (float)sum / size;
+}
+
+void displayArray(int arr[], int size) { // #Cp
+    printf("Array: ");
+    for (int i = 0; i < size; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void reverseArray(int arr[], int size) { // #Cp
+    for (int i = 0; i < size / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
+    }
+}
+
+void sortArray(int arr[], int size) { // #Cp
+    for (int i = 0; i < size - 1; i++)
+        for (int j = i + 1; j < size; j++)
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+}
+
+int linearSearch(int arr[], int size, int value) { // #Cp
+    for (int i = 0; i < size; i++)
+        if (arr[i] == value) return i;
+    return -1;
+}
