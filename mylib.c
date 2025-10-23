f72a1eed8d928c8c8318fbcd9a22dc61d0186b1a
rnsa357-stack <rnsa357@gmail.com>
2025-10-23 20:04:05 +0530

    Add mylib.c

---
+++ mylib.c
@@ -0,0 +1,39 @@
+#include "mylib.h"
+#include <math.h>
+
+// helper: reverse digits
+int reverseDigits(int n) {
+    int r = 0;
+    while (n) { r = r*10 + (n % 10); n /= 10; }
+    return r;
+}
+
+int isArmstrong(int num) {
+    int sum = 0, temp = num, digits = 0;
+    while (temp) { digits++; temp /= 10; }
+    temp = num;
+    while (temp) {
+        int d = temp % 10;
+        sum += pow(d, digits);
+        temp /= 10;
+    }
+    return sum == num;
+}
+
+int isAdams(int num) {
+    int sq = num * num;
+    int rev = reverseDigits(num);
+    int revsq = rev * rev;
+    return reverseDigits(sq) == revsq;
+}
+
+int isPrime(int num) {
+    if (num < 2) return 0;
+    for (int i = 2; i <= sqrt(num); i++)
+        if (num % i == 0) return 0;
+    return 1;
+}
+
+int isPrimePalindrome(int num) {
+    return isPrime(num) && (num == reverseDigits(num));
+}
