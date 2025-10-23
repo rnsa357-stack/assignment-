567bbd8d3210f4cdcfa73e15d0011bfa8a18d60c
rnsa357-stack <rnsa357@gmail.com>
2025-10-23 20:21:32 +0530

    Add mylib.c with Armstrong, Adams, and Prime Palindrome functions

--- mylib.c
+++ mylib.c
@@ -1,16 +1,23 @@
 #include "mylib.h"
 #include <math.h>
 
-// helper: reverse digits
+// helper: reverse digits of a number
 int reverseDigits(int n) {
     int r = 0;
-    while (n) { r = r*10 + (n % 10); n /= 10; }
+    while (n) {
+        r = r * 10 + (n % 10);
+        n /= 10;
+    }
     return r;
 }
 
+// Armstrong number check
 int isArmstrong(int num) {
     int sum = 0, temp = num, digits = 0;
-    while (temp) { digits++; temp /= 10; }
+    while (temp) {
+        digits++;
+        temp /= 10;
+    }
     temp = num;
     while (temp) {
         int d = temp % 10;
@@ -20,6 +27,7 @@ int isArmstrong(int num) {
     return sum == num;
 }
 
+// Adams number check
 int isAdams(int num) {
     int sq = num * num;
     int rev = reverseDigits(num);
@@ -27,13 +35,16 @@ int isAdams(int num) {
     return reverseDigits(sq) == revsq;
 }
 
+// Prime check (helper)
 int isPrime(int num) {
     if (num < 2) return 0;
-    for (int i = 2; i <= sqrt(num); i++)
+    for (int i = 2; i <= sqrt(num); i++) {
         if (num % i == 0) return 0;
+    }
     return 1;
 }
 
+// Prime Palindrome check
 int isPrimePalindrome(int num) {
     return isPrime(num) && (num == reverseDigits(num));
 }
