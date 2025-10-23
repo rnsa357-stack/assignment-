203f4d596b2650c601c77fabc9300d4a72c17d68
rnsa357-stack <rnsa357@gmail.com>
2025-10-23 21:34:14 +0530

    add araaylob.h

---
+++ arraylib.h
@@ -0,0 +1,12 @@
+#ifndef ARRAYLIB_H
+#define ARRAYLIB_H
+
+int findMaxIndex(int arr[], int size); // #Cp
+int findMinIndex(int arr[], int size); // #Cp
+float findAverage(int arr[], int size); // #Cp
+void displayArray(int arr[], int size); // #Cp
+void reverseArray(int arr[], int size); // #Cp
+void sortArray(int arr[], int size); // #Cp
+int linearSearch(int arr[], int size, int value); // #Cp
+
+#endif
